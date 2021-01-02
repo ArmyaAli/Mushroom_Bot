bfd912d28a1f42e0135ce67267bb0cd05ab27f60
armyaali <ali.umar.work@gmail.com>
2021-01-02 10:29:12 -0500

    organization

--- src/util/spotify-API-service.ts
+++ src/util/spotify-API-service.ts
@@ -1,6 +1,10 @@
-import { Agent } from "http";
 import fetch, { RequestInit } from "node-fetch";
-import { unregisterCustomQueryHandler } from "puppeteer";
+
+export interface SPOTIFY_PLAYLIST_SONG {
+  title: string;
+  artist: string;
+  album: string;
+}
 
 const authorize = async (): Promise<string | undefined> => {
   const ACCESS_TOKEN_URL = "https://accounts.spotify.com/api/token/";
@@ -16,7 +20,9 @@ const authorize = async (): Promise<string | undefined> => {
     method: "POST",
     headers: {
       "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-       "Authorization": `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64")}`,
+      Authorization: `Basic ${Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString("base64")}`,
     }, // request headers. format is the identical to that accepted by the Headers constructor (see below)
     body: params,
     compress: true,
@@ -30,29 +36,44 @@ const authorize = async (): Promise<string | undefined> => {
   }
 };
 
-export const grabAllSongsFromPlaylist = async (id: string | undefined): Promise<Array<Array<string | null>> | undefined> => {
-  if(id === undefined) return;
-  const URL = `https://api.spotify.com/v1/playlists/${id}?`;
-  const params = new URLSearchParams({
-    fields: "tracks.items",
-  });
+export const grabAllSongsFromPlaylist = async (
+  id: string | undefined
+): Promise<Array<SPOTIFY_PLAYLIST_SONG> | undefined> => {
+  if (id === undefined) return;
+  const URL = `https://api.spotify.com/v1/playlists/${id}/tracks?`;
+
   try {
+    const BEARER_TOKEN = await authorize();
     const options: RequestInit = {
       // These properties are part of the Fetch Standard
       method: "GET",
       headers: {
         "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Authorization": `Bearer ${await authorize()}`,
+        Authorization: `Bearer ${BEARER_TOKEN}`,
       }, // request headers. format is the identical to that accepted by the Headers constructor (see below)
       compress: true,
     };
 
-    const response = await fetch(URL + params, options);
-    const data = await response.json();
-    const titles: string[][] = data.tracks.items.map((item: any) => {
-      return [item.track.album.name, item.track.artists.map((artist: any) => artist.name).join(','), item.track.name];
-    });
-    return titles;
+    const songData: Array<SPOTIFY_PLAYLIST_SONG> = [];
+
+    let response = await fetch(URL, options);
+    let data = await response.json();
+    do {
+      data.items.forEach((song: any) => {
+        songData.push({
+          title: song.track.name,
+          artist: song.track.artists
+            .map((artist: any) => artist.name)
+            .join(","),
+          album: song.track.album.name,
+        });
+      });
+      if (data.next) {
+        response = await fetch(data.next, options);
+        data = await response.json();
+      }
+    } while (songData.length < data.total);
+    return songData;
   } catch (err) {
     console.log(`Error: ${err}`);
   }
