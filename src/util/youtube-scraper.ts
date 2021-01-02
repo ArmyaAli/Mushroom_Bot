7cd1e7494b20ff6aa927dd98f51670ef0bca9827
armyaali <ali.umar.work@gmail.com>
2021-01-02 10:28:12 -0500

    moved away from puppateer and used yt-search

--- src/util/youtube-scraper.ts
+++ src/util/youtube-scraper.ts
@@ -1,51 +1,33 @@
-import puppeteer from "puppeteer";
 import { search as YT_SEARCH } from "yt-search";
+import { SPOTIFY_PLAYLIST_SONG } from "./spotify-API-service";
 
-export const grabResultsFromPage = async(query: string) => {
-  const SEARCH_URL = "https://www.youtube.com/results?search_query=";
+export interface YT_SEARCH_VIDEO {
+  title: string;
+  url: string;
+}
 
+export const getURLFromQuery = async (query: string): Promise<YT_SEARCH_VIDEO | undefined> => {
   try {
-    const URL: string = SEARCH_URL + query;
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(URL);
-    await page.waitForSelector("#video-title");
-
-    const videoLinks = await page.evaluate(() => {
-      const data = Array.from(document.querySelectorAll('a#video-title')).map((value: Element) => {
-        return [value.getAttribute('title'), value.getAttribute('href')];
-      })
-      return data
-    });
-    await browser.close();
-    return videoLinks[0];
-  } catch (err) {
+      const results = await YT_SEARCH(query);
+      const firstVideo = results.videos[0];
+      const title = firstVideo.title;
+      const url = firstVideo.url;
+      return { title, url };
+    } catch (err) {
     console.log(`Error: ${err}`);
   }
 }
 
-export const spotifyPlaylistToYoutube = async (queries: string[]): Promise<Array<string | null> | undefined> => {
-  const SEARCH_URL = "https://www.youtube.com/results?search_query=";
+export const getURLQueueFromQueries = async (queries: SPOTIFY_PLAYLIST_SONG[], queue: any[]): Promise<void> => {
+  if(!queries) return;
   try {
-    const linksToCrawl = queries.map(q=> SEARCH_URL + q);
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    const URLQueue: Array<string | null> = [];
-    
-    for(const link of linksToCrawl) {
-      await page.goto(link);
-      await page.waitForSelector("#video-title");
-  
-      const relatedVid = await page.evaluate(() => {
-        const data = Array.from(document.querySelectorAll('a#video-title')).map((value: Element) => {
-          return value.getAttribute('href');
-        })[0];
-        return data
-      });
-      URLQueue.push(relatedVid);
+    for(const query of queries) {
+      const searchVal = `${query.title} ${query.artist} ${query.album}`;
+      const results = await YT_SEARCH(searchVal);
+      const result = results.videos[0].url
+      const title = results.videos[0].title
+      queue.push({title: title, url: result});
     }
-    await browser.close();
-    return URLQueue;
   } catch (err) {
     console.log(`Error: ${err}`);
   }
