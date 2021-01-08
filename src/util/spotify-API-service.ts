ff4b705d855b14d123a4ea62524ade2d3f1b0acb
armyaali <ali.umar.work@gmail.com>
2021-01-08 10:36:29 -0500

    formatting

--- src/util/spotify-API-service.ts
+++ src/util/spotify-API-service.ts
@@ -6,35 +6,35 @@ export interface SPOTIFY_PLAYLIST_SONG {
   album: string;
 }
 
-const authorize = async (): Promise<string | undefined> => {
-  const ACCESS_TOKEN_URL = "https://accounts.spotify.com/api/token/";
-  const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-  const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+  const authorize = async (): Promise<string | undefined> => {
+    const ACCESS_TOKEN_URL = "https://accounts.spotify.com/api/token/";
+    const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+    const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 
-  const params = new URLSearchParams({
-    grant_type: "client_credentials",
-  });
+    const params = new URLSearchParams({
+      grant_type: "client_credentials",
+    });
 
-  const options: RequestInit = {
-    // These properties are part of the Fetch Standard
-    method: "POST",
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-      Authorization: `Basic ${Buffer.from(
-        `${CLIENT_ID}:${CLIENT_SECRET}`
-      ).toString("base64")}`,
-    }, // request headers. format is the identical to that accepted by the Headers constructor (see below)
-    body: params,
-    compress: true,
+    const options: RequestInit = {
+      // These properties are part of the Fetch Standard
+      method: "POST",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        Authorization: `Basic ${Buffer.from(
+          `${CLIENT_ID}:${CLIENT_SECRET}`
+        ).toString("base64")}`,
+      }, // request headers. format is the identical to that accepted by the Headers constructor (see below)
+      body: params,
+      compress: true,
+    };
+    try {
+      const response = await fetch(ACCESS_TOKEN_URL, options);
+      const authenticated = await response.json();
+      return authenticated.access_token;
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
   };
-  try {
-    const response = await fetch(ACCESS_TOKEN_URL, options);
-    const authenticated = await response.json();
-    return authenticated.access_token;
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
-};
 
 export const grabAllSongsFromPlaylist = async (
   id: string | undefined
