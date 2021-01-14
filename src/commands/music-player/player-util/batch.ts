949e5a1e5b0aa7c2d3dabd58cd47b949b1885920
armyaali <ali.umar.work@gmail.com>
2021-01-14 12:40:59 -0500

    decouple batch from play command

---
+++ src/commands/music-player/player-util/batch.ts
@@ -0,0 +1,25 @@
+import { MusicStateManager } from "../../../util/StateManagement";
+import { SPOTIFY_PLAYLIST_SONG } from "./spotify-API-service";
+import { getURLQueueFromQueries } from "./youtube-scraper";
+
+export const batchQueue = async (
+    queries: SPOTIFY_PLAYLIST_SONG[],
+  ): Promise<void> => {
+    const batch: Array<SPOTIFY_PLAYLIST_SONG> = [];
+    if (!queries) return;
+    for (let i = 0; i < 5; ++i) {
+      const item = queries.shift();
+      if (item) {
+        batch.push(item);
+      }
+    }
+    try {
+      // batch of 5
+      console.log(batch);
+      await getURLQueueFromQueries(batch, MusicStateManager.musicQueue);
+  
+    } catch (error) {
+      console.log("Error batching");
+    }
+  };
+  
