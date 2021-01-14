9e2d787c6b743f15622a2ca814f87fb6d83af0dc
armyaali <ali.umar.work@gmail.com>
2021-01-14 12:41:29 -0500

    added flag check if we are currently batching

--- src/util/StateManagement.ts
+++ src/util/StateManagement.ts
@@ -1,17 +1,19 @@
 import { StreamDispatcher } from 'discord.js';
 import { EventEmitter } from 'events';
-import { YT_SEARCH_VIDEO } from './youtube-scraper';
+import { YT_SEARCH_VIDEO } from '../commands/music-player/player-util/youtube-scraper';
 
 
 class _MusicStateManager extends EventEmitter {
     musicQueue: YT_SEARCH_VIDEO[];
     playingMusic: boolean;
+    batching: boolean;
     dispatcher: StreamDispatcher | null;
     constructor() {
         super();
         this.musicQueue = [];
         this.playingMusic = false;
         this.dispatcher = null;
+        this.batching = false;
     }
 };
 export const MusicStateManager = new _MusicStateManager();
