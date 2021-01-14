6a1da88246c30995f6265a3662bfa4101e315efe
armyaali <ali.umar.work@gmail.com>
2021-01-13 23:49:45 -0500

    created a static ref to a stream dispatcher

--- src/util/StateManagement.ts
+++ src/util/StateManagement.ts
@@ -1,12 +1,15 @@
+import { StreamDispatcher } from 'discord.js';
 import { EventEmitter } from 'events';
 import { YT_SEARCH_VIDEO } from './youtube-scraper';
 
 export class MusicStateManager extends EventEmitter {
     static musicQueue: YT_SEARCH_VIDEO[];
     static playingMusic: boolean;
-    constructor () {
+    static dispatcher: StreamDispatcher | null;
+    constructor() {
         super();
         MusicStateManager.musicQueue = [];
         MusicStateManager.playingMusic = false;
+        MusicStateManager.dispatcher = null;
     }
 }
