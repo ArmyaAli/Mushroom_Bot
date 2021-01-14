d1d7592e79aed0b4bb3f9431d1383928e7413019
armyaali <ali.umar.work@gmail.com>
2021-01-14 12:25:45 -0500

    refactored export style

--- src/util/StateManagement.ts
+++ src/util/StateManagement.ts
@@ -2,14 +2,16 @@ import { StreamDispatcher } from 'discord.js';
 import { EventEmitter } from 'events';
 import { YT_SEARCH_VIDEO } from './youtube-scraper';
 
-export class MusicStateManager extends EventEmitter {
-    static musicQueue: YT_SEARCH_VIDEO[];
-    static playingMusic: boolean;
-    static dispatcher: StreamDispatcher | null;
+
+class _MusicStateManager extends EventEmitter {
+    musicQueue: YT_SEARCH_VIDEO[];
+    playingMusic: boolean;
+    dispatcher: StreamDispatcher | null;
     constructor() {
         super();
-        MusicStateManager.musicQueue = [];
-        MusicStateManager.playingMusic = false;
-        MusicStateManager.dispatcher = null;
+        this.musicQueue = [];
+        this.playingMusic = false;
+        this.dispatcher = null;
     }
-}
+};
+export const MusicStateManager = new _MusicStateManager();
