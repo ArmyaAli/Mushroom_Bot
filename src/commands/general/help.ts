701a4f1b54099f653fd1653db234e8c03f6ba91c
armyaali <ali.umar.work@gmail.com>
2021-01-14 14:53:48 -0500

    updated help text

--- src/commands/general/help.ts
+++ src/commands/general/help.ts
@@ -2,20 +2,21 @@ import { Client, Message } from "discord.js";
 import { Command } from "../../command";
 
 const helpTemplate = `
-  - Cool commands
-      - !ud *query* -> Search urbandictionary.com 
-      - !random -> grabs a random image off of flickr
-      - !random *search_token* -> grabs a random image off of flickr related to the search_token
-  - Music Player
-      - !play *songname* -> Plays a song!
-      - !queue -> returns all the songs in the queue listed out for you!
-      - !resume/pause -> plays/resumes the currently playing song
-      - !skip -> skips the current song in the queue
-      - !stop -> Stops playing the song and exits the voice channel
-  - Admin
-      - !kick @member -> kicks the server member if the author has the correct permissions
-      - !clear N -> deletes N number of messages up to a 100
-      - !clear nuke -> clears the entire text channel
+- Cool commands
+  - !ud *query* -> Search urbandictionary.com 
+  - !random -> grabs a random image off of flickr
+  - !random *search_token* -> grabs a random image off of flickr related to the search_token
+- Music Player 
+  - !play *songname* -> Plays a song! This also supports queuing up a spotify playlist! (WORKING)
+  - !queue -> returns the next 10 sonds in the music queue
+  - !resume/pause -> plays/resumes the currently playing song
+  - !skip -> skips the current song in the queue 
+  - !stop -> Stops playing the song and exits the voice channel 
+  - !clearq -> clears the music queue however keeps the current song playing
+- Admin
+  - !kick @member -> kicks the server member if the author has the correct permissions
+  - !clear N -> deletes N number of messages up to a 100
+  - !clear nuke -> clears the entire text channel
 `;
 
 // This will complain if you don't provide the right types for each property
