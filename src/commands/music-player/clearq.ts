843420d9b29aa169ba02491257c1d993b449a3e3
armyaali <ali.umar.work@gmail.com>
2021-01-14 12:24:42 -0500

    created clearq file

---
+++ src/commands/music-player/clearq.ts
@@ -0,0 +1,30 @@
+import { Client, Message, VoiceChannel, VoiceConnection } from "discord.js";
+import { Command } from "../../command";
+import { MusicStateManager } from "../../util/StateManagement";
+
+const command: Command = {
+    name: "clearq",
+    description: "clears the music queue!",
+    requiredPermissions: [],
+    async execute(client: Client, message: Message, args: string[]) {
+        const query = args.join(" ");
+        if (!message.member!.voice!.channel) {
+            await message.channel.send("You must be in a voice channel to use this command!");
+            return;
+        }
+        if (MusicStateManager.musicQueue.length !== 0) {
+            await message.channel.send("Cleared the Music Queue!");
+            MusicStateManager.dispatcher = null;
+            MusicStateManager.musicQueue = [];
+            MusicStateManager.emit("clear");
+        } else
+            await message.channel.send("Music Queue is already empty!");
+
+        try {
+        } catch (error) {
+            console.log(error);
+        }
+    },
+};
+
+export = command;
