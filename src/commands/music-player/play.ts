d680f7a6b84dc2633d628e8eb040a27d2cc24125
Ringo Chui <ringochewyis@gmail.com>
2021-01-29 19:43:18 -0500

    Built play command with DisTube

--- src/commands/music-player/play.ts
+++ src/commands/music-player/play.ts
@@ -1,14 +1,18 @@
 import { Client, Message, VoiceChannel, VoiceConnection } from "discord.js";
 import { Command } from "../../command";
+import DistubeManager from "../../util/distubeManager";
+
 
 const command: Command = {
   name: "play",
   description: "Searches youtube for a specified song and plays it. Plays spotify playlists as well.",
   requiredPermissions: [],
   async execute(client: Client, message: Message, args: string[]) {
-
-
-    
+    try {
+      await DistubeManager.Instance?.play(message, args.join(" "));
+    } catch(error) {
+      console.log(error)
+    }
   },
 };
 
