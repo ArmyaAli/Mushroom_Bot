d379a0904b14349e6285121f4e04af6b97e36186
ArmyaAli <ali.umar.work@gmail.com>
2021-02-10 00:06:34 -0500

    created method to register events and added some..

--- src/util/global-util/distubeManager.ts
+++ src/util/global-util/distubeManager.ts
@@ -1,10 +1,40 @@
 import { BaseClient, Client, Message, VoiceChannel, VoiceConnection } from "discord.js";
 import DisTube, { DisTubeOptions } from "distube";
+import Queue from "distube/typings/Queue";
+import Song from "distube/typings/Song";
 
 class _DistubeManager {
     Instance: DisTube | null;
+
     constructor() {
         this.Instance = null;
+    }
+
+    registerEvents(): void {
+        if (this.Instance) {
+
+            this.Instance.on("empty", (message: Message) => message.channel.send("Channel is empty. Leaving the channel"))
+            this.Instance.on("playSong", (message: Message, queue: Queue, song: Song) => {
+                const status = (queue: Queue) => {
+                    `Volume: \`${queue.volume}%\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ?
+                        "Server Queue" : "This Song" : "Off"}\` | Autoplay: \`$ {queue.autoplay ? "On" : "Off"}\``
+                }
+
+                message.channel.send(
+                    `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n`
+                )
+            });
+            this.Instance.on("addSong", (message: Message, queue: Queue, song: Song) => message.channel.send(
+                `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+            ));
+            this.Instance.on("error", (message: Message, err) => message.channel.send(
+                "An error encountered: " + err
+            ));
+            this.Instance.on("finish", (message: Message) => message.channel.send("No more song in queue"));
+
+
+        }
+
     }
 }
 
