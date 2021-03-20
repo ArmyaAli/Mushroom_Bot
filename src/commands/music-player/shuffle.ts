1033cee854877e28032e611ec52402df4b71d16b
ArmyaAli <ali.umar.work@gmail.com>
2021-03-20 10:03:25 -0400

    updated shuffle funciton for new music queue implementation

--- src/commands/music-player/shuffle.ts
+++ src/commands/music-player/shuffle.ts
@@ -1,21 +1,25 @@
-import { Client, Message, VoiceChannel, VoiceConnection } from "discord.js";
+import { Client, Message, MessageEmbed } from "discord.js";
 import Queue from "distube/typings/Queue";
 import { Command } from "../../command";
 import DistubeManager, { SongData } from "../../util/global-util/distubeManager";
+
+const shuffledEmbed = new MessageEmbed()
+    .setTitle('Shuffled All Songs in the Queue')
+    .setColor(0xff0000)
 
 // https://bost.ocks.org/mike/shuffle/
 const shuffle = (array: SongData[]) => {
     let m = array.length, t, i;
     while (m) {
-      i = Math.floor(Math.random() * m--);
-  
-      // And swap it with the current element.
-      t = array[m];
-      array[m] = array[i];
-      array[i] = t;
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
     }
     return array;
-  }
+}
 const command: Command = {
     name: "shuffle",
     description:
@@ -24,22 +28,24 @@ const command: Command = {
     async execute(client: Client, message: Message, args: string[]) {
         try {
             if (DistubeManager.Instance) {
-                if(DistubeManager.addingPlaylist) {
-                    shuffle(DistubeManager.musicQueue)
-                    return;
-                }
-                let queue: Queue = await DistubeManager.Instance.getQueue(
-                    message
-                );
-                if (queue.songs.length === 0) {
-                    message.channel.send("Currently no songs in the queue.");
-                } else {
-                    DistubeManager.Instance.shuffle(message);
-                    let output = "Next songs in Queue! (Up to the next 10)\n"
-                    for(let i = 0; i < 10 && i < queue.songs.length; ++i) {
-                        output +=`**${i+1}**. [${queue.songs[i].name}] <${queue.songs[i].url}> - \`${queue.songs[i].formattedDuration}\`\n`
+                if (DistubeManager.addingPlaylist) {
+                    if (DistubeManager.musicQueue.length === 0) {
+                        message.channel.send("Currently no songs in the queue.");
+                        return;
                     }
-                    message.channel.send(output)
+                    shuffle(DistubeManager.musicQueue);
+                    message.channel.send(shuffledEmbed);
+                    if (DistubeManager.musicQueue.length > 0) {
+                        const queue = DistubeManager.musicQueue;
+                        let output = "Next songs in Queue! (Up to the next 10)\n"
+                        for (let i = 0; i < 10 && i < queue.length; ++i) {
+                            output += `**${i + 1}**. ${queue[i].name} by ${queue[i].artist}\n`
+
+                        }
+                        message.channel.send(output)
+                    }
+                   
+                    return;
                 }
             }
         } catch (error) {
