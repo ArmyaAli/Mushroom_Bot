42c84ce2294309ea82c4843e608f5dfd9f90d278
ArmyaAli <ali.umar.work@gmail.com>
2021-03-23 11:27:47 -0400

    added coin flip feature with images

--- src/commands/misc/flip.ts
+++ src/commands/misc/flip.ts
@@ -2,14 +2,18 @@ import { Client, Message } from "discord.js";
 import { Command } from "../../command";
 
 const command: Command = {
-  name: "flip",
-  description: "Heads or Tails!",
-  requiredPermissions: [],
-  execute(client: Client, message: Message, args: string[])  {
-    const test = ["assets/head.jpg", "assets/tails.png"]
-    const random = Math.floor(Math.random() * 2)
-    message.channel.send({files: [test[random]]})
-  }
+    name: "flip",
+    description: "Heads or Tails!",
+    requiredPermissions: [],
+    async execute(client: Client, message: Message, args: string[]) {
+        const test = ["assets/head.jpg", "assets/tails.png"]
+        const random = Math.floor(Math.random() * 2)
+        try {
+            await message.channel.send({ files: [test[random]] })
+        } catch (err) {
+            console.log(`Error ocurred in FLIP: ${err}`)
+        }
+    }
 }
 
 export = command;
