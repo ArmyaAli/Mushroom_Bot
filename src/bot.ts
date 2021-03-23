fcd751e0d615c2d27ce632a744e38799d3ccdab5
ArmyaAli <ali.umar.work@gmail.com>
2021-03-23 09:11:47 -0400

    bot leaves empty voice channel after 30 sec

--- src/bot.ts
+++ src/bot.ts
@@ -1,65 +1,85 @@
-import Discord, { BitFieldResolvable, Message, PermissionString, TextChannel } from "discord.js";
+import Discord, { BitFieldResolvable, Message, PermissionString, TextChannel, VoiceState } from "discord.js";
 import DistubeManager from "./util/global-util/distubeManager";
 import path from "path";
 import config, { readCommandsRecursive, distubeConfig } from "./botconfig";
-import {Command} from "./command";
+import { Command } from "./command";
 import DisTube from "distube";
 
 const client: Discord.Client = new Discord.Client();
 const commands: Discord.Collection<string, Command> = new Discord.Collection();
 const commandContext = path.join(__dirname, "commands");
-
+let s_message: Message | null = null
 let commandFiles: string[] = [];
 
 client.once("ready", async () => {
-  /* Set our music player */
-  DistubeManager.Instance = new DisTube(client, distubeConfig);
-  /* Register music player events */ 
-  DistubeManager.registerEvents();
-  readCommandsRecursive(commandContext, commandFiles);
-  commandFiles = commandFiles.filter((fileName) => fileName.endsWith(".ts") || fileName.endsWith(".js"));
-  for (const filePath of commandFiles) {
-    const command = require(filePath);
-    commands.set(command.name, command);
-  }
+    /* Set our music player */
+    DistubeManager.Instance = new DisTube(client, distubeConfig);
+    /* Register music player events */
+    DistubeManager.registerEvents();
+    readCommandsRecursive(commandContext, commandFiles);
+    commandFiles = commandFiles.filter((fileName) => fileName.endsWith(".ts") || fileName.endsWith(".js"));
+    for (const filePath of commandFiles) {
+        const command = require(filePath);
+        commands.set(command.name, command);
+    }
 });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user!.tag}!`);
+    console.log(`Logged in as ${client.user!.tag}!`);
 });
 
-client.on("message", async (message : Message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/);
-  const command: string = args.shift()!.toLowerCase();
+client.on("message", async (message: Message) => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+    const command: string = args.shift()!.toLowerCase();
+    s_message = message;
 
-  if (!commands.get(command)) {
-    message.channel.send("That is an invalid command");
-    return;
-  }
+    if (!commands.get(command)) {
+        message.channel.send("That is an invalid command");
+        return;
+    }
 
-  if(message.member) {
-    if (message.member.hasPermission(commands.get(command)!.requiredPermissions)) {
-      commands.get(command)!.execute(client, message, args);
-    } else
-      message.channel.send("You do not have the required permissions to execute that command!")
-  }
+    if (message.member) {
+        if (message.member.hasPermission(commands.get(command)!.requiredPermissions)) {
+            commands.get(command)!.execute(client, message, args);
+        } else
+            message.channel.send("You do not have the required permissions to execute that command!")
+    }
 
-  
+
 });
 
 // sends a welcome message if a user joins
 client.on("guildMemberAdd", (member) => {
-  const channel = member.guild.channels.cache.find(
-    (ch) => ch.id == `143853351103102976`
-  );
-  if (!channel) return;
-  (channel as TextChannel).send(
-    "Welcome to the Mushroom Cave" + " " + member.displayName + "!"
-  );
+    const channel = member.guild.channels.cache.find(
+        (ch) => ch.id == `143853351103102976`
+    );
+    if (!channel) return;
+    (channel as TextChannel).send(
+        "Welcome to the Mushroom Cave" + " " + member.displayName + "!"
+    );
+});
+
+client.on("voiceStateUpdate", (oldState: VoiceState, newState: VoiceState) => {
+    const oldSize = oldState.channel?.members.size;
+    const newSize = newState.channel?.members.size;
+    console.log(oldSize, newSize)
+    if (oldSize) {
+        if (oldSize <= 1) {
+            s_message?.channel.send("Empty Voice Channel...Leaving the Voice Channel in 30 seconds...")
+            setTimeout(() => {
+                DistubeManager.musicQueue = [];
+                DistubeManager.firstAuthor = undefined;
+                DistubeManager.addingPlaylist = false;
+                DistubeManager.currentSong = null;
+                oldState.channel?.leave();
+            }, 30000)
+        }
+    }
 });
 
 client.login(config.token).catch(error => {
     console.log(`Error occured during login: ${error}`);
+
     process.exit();
 });
