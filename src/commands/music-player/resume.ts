d0674e0a1d634af0a827d2fb5040ff74cecd29ac
ArmyaAli <ali.umar.work@gmail.com>
2021-03-23 12:16:41 -0400

    implemented resume with multiguild queue

--- src/commands/music-player/resume.ts
+++ src/commands/music-player/resume.ts
@@ -1,32 +1,36 @@
-// import { Client, Message, VoiceChannel, VoiceConnection } from "discord.js";
-// import Queue from "distube/typings/Queue";
-// import { Command } from "../../command";
-// import MusicManager from "../../util/global-util/MusicManager";
+import { Client, Message, VoiceChannel, VoiceConnection } from "discord.js";
+import Queue from "distube/typings/Queue";
+import { Command } from "../../command";
+import MusicManager from "../../util/music-player-util/musicManager";
 
 
-// const command: Command = {
-//     name: "resume",
-//     description: "Continues current song in the queue.",
-//     requiredPermissions: [],
-//     async execute(client: Client, message: Message, args: string[]) {
-//         try {
-//             if (MusicManager.Instance) {
-//                 let queue: Queue = await MusicManager.Instance.getQueue(message);
-//                 if (queue == undefined) {
-//                     message.channel.send('Currently no songs in the queue.');
-//                 }
-//                 else if (MusicManager.Instance.isPlaying(message)) {
-//                     message.channel.send('Song is currently playing.');
-//                 }
-//                 else {
-//                     await MusicManager.Instance.resume(message);
-//                     message.channel.send('Continuing song.');
-//                 }
-//             }
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     },
-// };
+const command: Command = {
+    name: "resume",
+    description: "Continues current song in the queue.",
+    requiredPermissions: [],
+    async execute(client: Client, message: Message, args: string[]) {
+        try {
+            const GUILD_ID = message.guild?.id;
+            if (GUILD_ID) {
+                const player = MusicManager.musicQueue.get(GUILD_ID);
+                if (player) {
+                    let queue: Queue = await player.Instance.getQueue(message);
+                    if (queue == undefined) {
+                        message.channel.send('Currently no songs in the queue.');
+                    }
+                    else if (player.Instance.isPlaying(message)) {
+                        message.channel.send('Song is currently playing.');
+                    }
+                    else {
+                        await player.Instance.resume(message);
+                        message.channel.send('Continuing song.');
+                    }
+                }
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    },
+};
 
-// export = command;
+export = command;
