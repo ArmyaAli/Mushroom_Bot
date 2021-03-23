98a578c005016e40cd3380d842814a1cd23ef059
ArmyaAli <ali.umar.work@gmail.com>
2021-03-23 14:27:08 -0400

    added  voice channel leaving, deleting music queue

--- src/bot.ts
+++ src/bot.ts
@@ -1,19 +1,16 @@
-import Discord, { BitFieldResolvable, Message, PermissionString, TextChannel, VoiceState } from "discord.js";
+import Discord, { Message, TextChannel, VoiceState } from "discord.js";
 import path from "path";
-import config, { readCommandsRecursive, distubeConfig } from "./botconfig";
+import config, { readCommandsRecursive } from "./botconfig";
 import { Command } from "./command";
+import MusicManager from "./util/music-player-util/musicManager";
 
 const client: Discord.Client = new Discord.Client();
 const commands: Discord.Collection<string, Command> = new Discord.Collection();
 const commandContext = path.join(__dirname, "commands");
-let s_message: Message | null = null
+let latestMessage: Message | null = null
 let commandFiles: string[] = [];
 
 client.once("ready", async () => {
-    /* Set our music player */
-    // MusicManager.Instance = new DisTube(client, distubeConfig);
-    /* Register music player events */
-    // MusicManager.registerEvents();
     readCommandsRecursive(commandContext, commandFiles);
     commandFiles = commandFiles.filter((fileName) => fileName.endsWith(".ts") || fileName.endsWith(".js"));
     for (const filePath of commandFiles) {
@@ -30,7 +27,7 @@ client.on("message", async (message: Message) => {
     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/);
     const command: string = args.shift()!.toLowerCase();
-    s_message = message;
+    latestMessage = message;
 
     if (!commands.get(command)) {
         message.channel.send("That is an invalid command");
@@ -58,28 +55,27 @@ client.on("guildMemberAdd", (member) => {
     );
 });
 
-// client.on("voiceStateUpdate", (oldState: VoiceState, newState: VoiceState) => {
-//     const oldSize = oldState.channel?.members.size;
-//     const newSize = newState.channel?.members.size;
-//     const currentChannelId = oldState.channelID;
-//     const currentGuild = oldState.guild;
-//     if (currentChannelId) {
-//         const currentChannel = oldState.guild.channels.cache.get(currentChannelId)
-//         if (oldSize && currentGuild.channels.) {
-//             if (oldSize <= 1) {
-//                 s_message?.channel.send("Empty Voice Channel...Leaving the Voice Channel in 30 seconds...")
-//                 setTimeout(() => {
-//                     // MusicManager.musicQueue = [];
-//                     // MusicManager.firstAuthor = undefined;
-//                     // MusicManager.addingPlaylist = false;
-//                     // MusicManager.currentSong = null;
-//                     oldState.channel?.leave();
-//                 }, 30000)
-//             }
-//         }
-//     }
+client.on("voiceStateUpdate", (oldState: VoiceState, newState: VoiceState) => {
+    const oldChannel = oldState.channel;
+    const oldGuild = oldState.guild;
+    if (oldChannel && oldGuild) {
+        if (oldChannel.members.size === 1) {
+            if (latestMessage) {
+                const channelToMessage = oldGuild.channels.cache.get(latestMessage.channel.id) as TextChannel;
+                channelToMessage.send("Empty channel. Leaving in 30 seconds!! Mushroomie ~~");
+            }
 
-// });
+            setTimeout(() => {
+                const toDispose = MusicManager.musicQueue.get(oldGuild.id);
+
+                if (toDispose) {
+                    MusicManager.musicQueue.delete(oldGuild.id);
+                }
+                oldState.channel?.leave();
+            }, 30000)
+        }
+    }
+});
 
 client.login(config.token).catch(error => {
     console.log(`Error occured during login: ${error}`);
