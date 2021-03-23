8963778c7f763b7d3b4a9d3cac9c709712057d1c
ArmyaAli <ali.umar.work@gmail.com>
2021-03-23 15:03:29 -0400

    implemented stop with multiguild queue

--- src/commands/music-player/stop.ts
+++ src/commands/music-player/stop.ts
@@ -1,29 +1,35 @@
-// import { Client, Message, MessageEmbed } from "discord.js";
-// import Queue from "distube/typings/Queue";
-// import { Command } from "../../command";
-// import MusicManager from "../../util/global-util/MusicManager";
+import { Client, Message, MessageEmbed } from "discord.js";
+import Queue from "distube/typings/Queue";
+import { Command } from "../../command";
+import MusicManager from "../../util/music-player-util/musicManager";
 
-// const command: Command = {
-//     name: "stop",
-//     description: "Displays the current queue of the DisTube music player.",
-//     requiredPermissions: [],
-//     async execute(client: Client, message: Message, args: string[]) {
-//         const stoppedPlaylist = new MessageEmbed()
-//             .setTitle('Stopped adding the playlist')
-//             .setColor(0xff0000)
-//         try {
-//             if (MusicManager.Instance) {
-//                 MusicManager.Instance.stop(message);
-//                 MusicManager.musicQueue = []
-//                 MusicManager.addingPlaylist = false;
-//                 MusicManager.currentSong = null;
-//                 MusicManager.firstAuthor = undefined;
-//                 await message.channel.send(stoppedPlaylist)
-//             }
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     },
-// };
+const command: Command = {
+    name: "stop",
+    description: "Displays the current queue of the DisTube music player.",
+    requiredPermissions: [],
+    async execute(client: Client, message: Message, args: string[]) {
+        const stoppedPlaylist = new MessageEmbed()
+            .setTitle('Stopped adding the playlist')
+            .setColor(0xff0000)
+        try {
+            const GUILD_ID = message.guild?.id;
+            if (GUILD_ID) {
+                const player = MusicManager.musicQueue.get(GUILD_ID);
+                if (player) {
+                    if (player.Instance) {
+                        player.Instance.stop(message);
+                        player.Queue = []
+                        player.addingPlaylist = false;
+                        player.currentSong = null;
+                        player.firstAuthor = undefined;
+                        await message.channel.send(stoppedPlaylist)
+                    }
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+};
 
-// export = command;
+export = command;
