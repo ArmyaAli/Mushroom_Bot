afbd1c0e04dfd241f9ad1f432045f856b57008c8
ArmyaAli <ali.umar.work@gmail.com>
2021-03-23 12:16:56 -0400

    implemented skip with mutli guild queue

--- src/commands/music-player/skip.ts
+++ src/commands/music-player/skip.ts
@@ -1,34 +1,38 @@
-// import { Client, Message, VoiceChannel, VoiceConnection } from "discord.js";
-// import Queue from "distube/typings/Queue";
-// import { Command } from "../../command";
-// import MusicManager from "../../util/global-util/MusicManager";
+import { Client, Message, VoiceChannel, VoiceConnection } from "discord.js";
+import Queue from "distube/typings/Queue";
+import { Command } from "../../command";
+import MusicManager from "../../util/music-player-util/musicManager";
 
-// const command: Command = {
-//     name: "skip",
-//     description: "Skips the current song playing in the queue.",
-//     requiredPermissions: [],
-//     async execute(client: Client, message: Message, args: string[]) {
-//         try {
-//             if (MusicManager.Instance) {
-//                 if (MusicManager.musicQueue.length > 0) {
-//                     const next = MusicManager.musicQueue.shift()
-//                     if (next) {
-//                         MusicManager.currentSong = next;
-//                         await MusicManager.Instance.playSkip(message, next.url)
-//                         MusicManager.Instance.getQueue(message).autoplay = true;
-//                     }
-//                     return;
-//                 }
+const command: Command = {
+    name: "skip",
+    description: "Skips the current song playing in the queue.",
+    requiredPermissions: [],
+    async execute(client: Client, message: Message, args: string[]) {
+        try {
+            const GUILD_ID = message.guild?.id;
+            if (GUILD_ID) {
+                const player = MusicManager.musicQueue.get(GUILD_ID);
+                if (player) {
+                    if (player.Queue.length > 0) {
+                        const next = player.Queue.shift()
+                        if (next) {
+                            player.currentSong = next;
+                            await player.Instance.playSkip(message, next.url)
+                            player.Instance.getQueue(message).autoplay = true;
+                        }
+                        return;
+                    }
 
-//                 message.channel.send("Currently no songs in the queue. Defaulting to playing the next related song...");
-//                 await MusicManager.Instance.addRelatedVideo(message)
-//                 MusicManager.Instance.skip(message)
-                
-//             };
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     },
-// };
+                    message.channel.send("Currently no songs in the queue. Defaulting to playing the next related song...");
+                    await player.Instance.addRelatedVideo(message)
+                    player.Instance.skip(message)
 
-// export = command;
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+};
+
+export = command;
