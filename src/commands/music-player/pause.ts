e36bce401421b5f6d2d8f9f95b71a3b2cd329ed3
ArmyaAli <ali.umar.work@gmail.com>
2021-03-23 11:28:42 -0400

    Distube -> MusicManager

--- src/commands/music-player/pause.ts
+++ src/commands/music-player/pause.ts
@@ -1,31 +1,31 @@
-import { Client, Message, VoiceChannel, VoiceConnection } from "discord.js";
-import Queue from "distube/typings/Queue";
-import { Command } from "../../command";
-import DistubeManager from "../../util/global-util/distubeManager";
+// import { Client, Message, VoiceChannel, VoiceConnection } from "discord.js";
+// import Queue from "distube/typings/Queue";
+// import { Command } from "../../command";
+// import MusicManager from "../../util/global-util/MusicManager";
 
-const command: Command = {
-    name: "pause",
-    description: "Stops current song in the queue.",
-    requiredPermissions: [],
-    async execute(client: Client, message: Message, args: string[]) {
-        try {
-            if (DistubeManager.Instance) {
-                let queue: Queue = DistubeManager.Instance.getQueue(
-                    message
-                );
-                if (queue == undefined) {
-                    message.channel.send("Currently no songs in the queue.");
-                } else if (DistubeManager.Instance.isPaused(message)) {
-                    message.channel.send("Song is currently paused.");
-                } else {
-                    DistubeManager.Instance.pause(message);
-                    message.channel.send("Pausing song.");
-                }
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    },
-};
+// const command: Command = {
+//     name: "pause",
+//     description: "Stops current song in the queue.",
+//     requiredPermissions: [],
+//     async execute(client: Client, message: Message, args: string[]) {
+//         try {
+//             if (MusicManager.Instance) {
+//                 let queue: Queue = MusicManager.Instance.getQueue(
+//                     message
+//                 );
+//                 if (queue == undefined) {
+//                     message.channel.send("Currently no songs in the queue.");
+//                 } else if (MusicManager.Instance.isPaused(message)) {
+//                     message.channel.send("Song is currently paused.");
+//                 } else {
+//                     MusicManager.Instance.pause(message);
+//                     message.channel.send("Pausing song.");
+//                 }
+//             }
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     },
+// };
 
-export = command;
+// export = command;
