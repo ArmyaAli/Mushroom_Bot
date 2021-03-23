6f0f4a93932ebb805f2741716bdb490d7d0191d9
ArmyaAli <ali.umar.work@gmail.com>
2021-03-23 12:16:32 -0400

    implemented queue with multiguild queue

--- src/commands/music-player/queue.ts
+++ src/commands/music-player/queue.ts
@@ -1,33 +1,37 @@
-// import { Client, Message, MessageFlags, VoiceChannel, VoiceConnection } from "discord.js";
-// import Queue from "distube/typings/Queue";
-// import { Command } from "../../command";
-// import MusicManager from "../../util/global-util/MusicManager";
+import { Client, Message, MessageFlags, VoiceChannel, VoiceConnection } from "discord.js";
+import Queue from "distube/typings/Queue";
+import { Command } from "../../command";
+import MusicManager from "../../util/music-player-util/musicManager";
 
-// const command: Command = {
-//     name: "queue",
-//     description: "Displays the current queue of the DisTube music player.",
-//     requiredPermissions: [],
-//     async execute(client: Client, message: Message, args: string[]) {
-//         try {
-//             if (MusicManager.Instance) {
-//                 if (MusicManager.musicQueue.length > 0) {
-//                     const queue = MusicManager.musicQueue;
-//                     let output = "Next songs in Queue! (Up to the next 10)\n"
-//                     for (let i = 0; i < 10 && i < queue.length; ++i) {
-//                         output += `**${i + 1}**. ${queue[i].name} by ${queue[i].artist}\n`
+const command: Command = {
+    name: "queue",
+    description: "Displays the current queue of the DisTube music player.",
+    requiredPermissions: [],
+    async execute(client: Client, message: Message, args: string[]) {
+        try {
+            const GUILD_ID = message.guild?.id;
+            if (GUILD_ID) {
+                const player = MusicManager.musicQueue.get(GUILD_ID);
+                if (player) {
+                    if (player.Queue.length > 0) {
+                        const queue = player.Queue;
+                        let output = "Next songs in Queue! (Up to the next 10)\n"
+                        for (let i = 0; i < 10 && i < queue.length; ++i) {
+                            output += `**${i + 1}**. ${queue[i].name} by ${queue[i].artist}\n`
 
-//                     }
-//                     output += `There are ${MusicManager.musicQueue.length} total songs in the Queue`
-//                     message.channel.send(output)
-//                     return;
-//                 }
-//                 await message.channel.send(`Queue is not avaliable yet or does not exist!`)
-//             }
+                        }
+                        output += `There are ${player.Queue.length} total songs in the Queue`
+                        message.channel.send(output)
+                        return;
+                    }
+                    await message.channel.send(`Queue is not avaliable yet or does not exist!`)
+                }
+            }
 
-//         } catch(error) {
-//         console.log(error);
-//     }
-// },
-// };
+        } catch (error) {
+            console.log(error);
+        }
+    },
+};
 
-// export = command;
+export = command;
