768586bfed53c572a3a263d8824753a1cecb46f4
ArmyaAli <ali.umar.work@gmail.com>
2021-03-29 19:35:23 -0400

    power

--- src/commands/misc/diw.ts
+++ src/commands/misc/diw.ts
@@ -2,12 +2,12 @@ import { Client, Message } from "discord.js";
 import { Command } from "../../command";
 
 const command: Command = {
-    name: "diw",
-    description: "Texts to speech, danny is a weeb!",
+    name: "power",
+    description: "TTS",
     requiredPermissions: [],
     async execute(client: Client, message: Message, args: string[]) {
         try {
-            await message.channel.send("Danny loves body pillows", {tts: true})
+            await message.channel.send("supa hot fire", {tts: true})
         } catch (err) {
             console.log(`Error ocurred in DIW: ${err}`)
         }
