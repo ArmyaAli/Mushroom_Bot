2e27d9f95ab2ddefcbb84a4069089b8eb959f599
ArmyaAli <ali.umar.work@gmail.com>
2021-06-23 22:52:13 -0400

    renamed to playerAPI

--- src/commands/music-player/playerAPI.ts
+++ src/commands/music-player/playerAPI.ts
@@ -1,4 +1,5 @@
 import { Client, Message } from "discord.js";
+import yts from "yt-search";
 
 export const checkVoiceStatus = (client: Client, message: Message) => {
     let inVoice = true;
@@ -20,5 +21,15 @@ export const checkVoiceStatus = (client: Client, message: Message) => {
         }
     } catch (err) {
         console.log(`Procedure [inVoice] in Play command, Error: ${err}`)
+    }
+}
+
+export const getFirstThreeSearchResults = async (query: string) => {
+    try {
+        const results = await yts(query);
+        const firstThree = results.videos.slice(0, 3);
+        return firstThree.map((video) => { return {title: video.title, url: video.url } });
+    } catch (err) {
+        console.error(`Procedure [getFirstThreeSearchResults] error: ${err}`);
     }
 }
