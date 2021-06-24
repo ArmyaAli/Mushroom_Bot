a2f25308974bc099791d9a46be2ac0e5b96adacc
ArmyaAli <ali.umar.work@gmail.com>
2021-06-23 21:41:35 -0400

    removed musicManager

--- src/bot.ts
+++ src/bot.ts
@@ -2,7 +2,6 @@ import Discord, { Message, TextChannel, VoiceState } from "discord.js";
 import path from "path";
 import config, { readCommandsRecursive } from "./botconfig";
 import { Command } from "./command";
-import MusicManager from "./util/music-player-util/musicManager";
 
 const client: Discord.Client = new Discord.Client();
 const commands: Discord.Collection<string, Command> = new Discord.Collection();
@@ -48,28 +47,6 @@ client.once("ready", async () => {
         console.log(`Event Proc [client.on("message")] threw error: ${err}`)
     }
 
-}).on("voiceStateUpdate", async (oldState: VoiceState, newState: VoiceState) => {
-    try {
-        const oldChannel = oldState.channel;
-        const oldGuild = oldState.guild;
-
-        /* This will check if a channel in the specific guild that the Specific Music Queue in the map is empty and leaves the voice channel is it becomes empty. Maybe we can improve it in the sense that it does not leave the voice if somebody comes back */
-        if (oldChannel && oldGuild) {
-            if (oldChannel.members.size === 1) { // if the bot is the only one in the channel
-                const toDispose = MusicManager.musicQueue.get(oldGuild.id);
-                if (toDispose) {
-                    await toDispose.message.channel.send("Empty channel. Leaving in 15 seconds!! Mushroomie ~~");
-
-                    setTimeout(() => {
-                        MusicManager.musicQueue.delete(oldGuild.id);
-                        oldState.channel?.leave()
-                    }, 15000)
-                }
-            }
-        }
-    } catch (err) {
-        console.log(`Event Proc [client.on("voiceStateUpdate")] threw error: ${err}`)
-    }
 });
 
 client.login(config.token).catch(error => {
