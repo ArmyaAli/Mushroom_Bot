e13ea34201a166f16e465a33d3c1073d31bf17fe
ArmyaAli <ali.umar.work@gmail.com>
2021-06-23 21:41:55 -0400

    deleted files for new design

--- src/commands/music-player/play.ts
+++ src/commands/music-player/play.ts
@@ -1,74 +1,7 @@
 import { Client, Guild, GuildMember, Message, MessageEmbed, User } from "discord.js";
-import { grabAllSongsFromPlaylist } from '../../util/music-player-util/spotify-API-service';
 import { Command } from "../../command";
-import MusicManager, { musicPlayer } from "../../util/music-player-util/musicManager";
-import { distubeConfig } from "../../botconfig"
-import yts from 'yt-search'
-import DisTube from "distube";
 import { checkVoiceStatus } from "./common";
 
-const finishedPlaylist = new MessageEmbed()
-    .setTitle('Spotify Playlist Fully Added!')
-    .setColor(0xff0000)
-
-const addSong = async (player: musicPlayer, message: Message, query: string, author: User | null) => {
-    if (player.Instance) {
-        const searchResult = await yts(query);
-        const songTitle = searchResult.videos[0].title
-        const songLink = searchResult.videos[0].url
-        player.Queue.push({ name: songTitle, url: songLink, requestedBy: author })
-        message.channel.send(
-            `Adding \`${songTitle}\` to the Queue\nRequested by: ${author}`
-        )
-    }
-}
-/* Ads the rest of the song to the distube queue */
-const addRestOfSongs = async (player: musicPlayer, message: Message, RAW_SONGS: string[], author: User | null) => {
-    if (player.Instance) {
-
-        for (let i = 0; i < RAW_SONGS.length; ++i) {
-            if (!player.addingPlaylist) break;
-            const name = RAW_SONGS[i].split(',')[0]
-            const artist = RAW_SONGS[i].split(',')[1]
-            const query = await yts(name + " " + artist);
-            player.Queue.push({ name: name, artist: artist, url: query.videos[0].url, requestedBy: author })
-        }
-
-        if (!player.addingPlaylist) {
-            player.Queue = []
-            return;
-        }
-
-        await message.channel.send(finishedPlaylist);
-        player.addingPlaylist = false;
-    }
-}
-
-const assignQueue = (client: Client, message: Message) => {
-    try {
-        const GUILD_ID = message.guild?.id;
-        if (GUILD_ID) {
-            if (MusicManager.musicQueue.has(GUILD_ID)) return;
-
-            MusicManager.musicQueue.set(GUILD_ID,
-                {
-                    Instance: new DisTube(client, distubeConfig),
-                    Queue: [],
-                    currentSong: null,
-                    firstAuthor: message.author,
-                    addingPlaylist: false,
-                    message: message
-                });
-
-            const thisQueue = MusicManager.musicQueue.get(GUILD_ID)
-
-            if (thisQueue)
-                MusicManager.registerEvents(thisQueue);
-        }
-    } catch (err) {
-        console.log(`Procedure [assignQueue] in Play command, Error: ${err}`)
-    }
-}
 
 const command: Command = {
     name: "play",
@@ -76,70 +9,7 @@ const command: Command = {
     requiredPermissions: [],
     async execute(client: Client, message: Message, args: string[]) {
         if (!checkVoiceStatus(client, message)) return;
-        // we need to assign the the command callee a Queue (if they don't have one already)
-        assignQueue(client, message);
-
-        try {
-            const GUILD_ID = message.guild?.id;
-            let author = message.member?.user
-            if (GUILD_ID) {
-                const player = MusicManager.musicQueue.get(GUILD_ID);
-                if (player) {
-                    const query = args.join(" ");
-                    if (query.startsWith('https://open.spotify.com/playlist')) {
-                        if (player.addingPlaylist) {
-                            message.channel.send(`There is already a playlist being processed. Wait until it is finished or use !stop and the Queue a new one.`)
-                            return;
-                        }
-                        const LIST_ID: string = query.substr(query.lastIndexOf('/') + 1);
-                        const RAW_SONGS: string[] | undefined = await grabAllSongsFromPlaylist(LIST_ID);
-
-                        if (RAW_SONGS) {
-                            player.addingPlaylist = true;
-
-                            if (player.Queue.length > 0 || player.Instance.isPlaying) {
-                                if (author)
-                                    addRestOfSongs(player, message, RAW_SONGS, author);
-                                return;
-                            }
-
-                            const firstSong = RAW_SONGS.shift()!.split(',').join(" ")
-                            const first = await yts(firstSong);
-                            const song = first.videos[0].url
-                            await player.Instance.play(message, song);
-
-                            if (author)
-                                addRestOfSongs(player, message, RAW_SONGS, author);
-                            return;
-                        }
-
-                        await message.channel.send(`Failed to add the Spotify Songs to the Music Queue.`);
-
-                    } else {
-                        if (player.Instance.isPlaying(message)) {
-                            await addSong(player, message, query, author ?? null)
-
-                            if (player.autoplay === true)
-                                player.autoplay = player.Instance.toggleAutoplay(message);
-
-                            console.log(`Hit the playing, autoplay ${player.autoplay}`)
-                            return;
-                        }
-
-                        await player.Instance.play(message, query);
-
-                        if (player.autoplay === false && player.Queue.length === 0) {
-                            player.autoplay = player.Instance.toggleAutoplay(message);
-                        }
-                        console.log(`Hit the not playing, autoplay ${player.autoplay}`)
-                    }
-                }
-            }
-
-        } catch (error) {
-            console.log(error)
-        }
-    },
+    }
 };
 
 export default command;
