e9626288c69660358a62665936dc0c38f764f214
ArmyaAli <ali.umar.work@gmail.com>
2021-06-27 12:47:23 -0400

    user message formatting

--- src/commands/music-player/skip.ts
+++ src/commands/music-player/skip.ts
@@ -1,7 +1,7 @@
 import { Client, Guild, GuildMember, Message, MessageEmbed, User } from "discord.js";
 import ytdl from "ytdl-core";
 import { Command } from "../../command";
-import { assignQueue, checkVoiceStatus, onSongFinish } from "./playerAPI";
+import { assignQueue, checkVoiceStatus, onSongFinish, TimeFormat } from "./playerAPI";
 import { Player } from "./playerState";
 
 const command: Command = {
@@ -13,28 +13,28 @@ const command: Command = {
         const guildId = message.guild?.id;
         try {
             if (guildId) {
-
                 const player = Player.GuildQueues.get(guildId);
                 if (player) {
-
-                    if(player.musicQueue.length === 0) {
+                    if (player.musicQueue.length === 0) {
                         message.channel.send("There are no songs Queued up!");
                         return;
                     }
-
                     const connection = await message.member?.voice.channel?.join();
-
                     if (connection) {
+                        const user = message.author;
                         const musicQueue = player.musicQueue;
                         const next = musicQueue.shift();
-                    
+
                         if (next) {
-                            const video = ytdl(next, { filter: 'audioonly' });
+                            const video = await ytdl(next.url, { filter: 'audioonly', dlChunkSize: 0 });
+                            player.currentSong = await ytdl.getInfo(next.url);
                             const dispatcher = connection.play(video); // first one 
                             player.playingMusic = true;
-                            dispatcher?.on("finish", () => onSongFinish(player, connection));
+                            dispatcher?.on("finish", () => onSongFinish(player, connection))
+                                .on('start', () => {
+                                    message.channel.send(`Playing \`${player.currentSong?.videoDetails.title}\` - \`${TimeFormat(parseInt(player.currentSong?.videoDetails.lengthSeconds ?? "0"))}\`Requested by: ${user ?? 'unknown'}`);
+                                })
                         }
-
                     }
                 }
             }
