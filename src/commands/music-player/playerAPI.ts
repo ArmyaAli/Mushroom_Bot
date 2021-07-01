e710fdf79f621fdf348440bb7857da213516722c
ArmyaAli <ali.umar.work@gmail.com>
2021-07-01 09:57:30 -0400

    added spotify playlist functionality

--- src/commands/music-player/playerAPI.ts
+++ src/commands/music-player/playerAPI.ts
@@ -1,4 +1,4 @@
-import { Client, Message } from "discord.js";
+import { Client, Message, User, VoiceConnection } from "discord.js";
 import yts from "yt-search";
 import ytdl from "ytdl-core";
 import { MusicPlayer, Player } from "./playerState";
@@ -59,24 +59,54 @@ export const getFirstThreeSearchResults = async (query: string) => {
     }
 }
 
+export const mapSongTitlesToYoutube = async (player: MusicPlayer, songs: string[], requestedBy: User) => {
+    try {
+        for (const song of songs) {
+            const results = await yts(song);
+            const first = results.videos[0];
+            player.musicQueue.push({ title: first.title, url: first.url, requestedBy: requestedBy });
+        }
+
+        player.message.channel.send(`Finished queuing up the spotify playlist, requested by ${requestedBy}`);
+    } catch (err) {
+        console.error(`Procedure [getFirstSearchResult] error: ${err}`);
+    }
+}
+
+export const play = async (player: MusicPlayer, results: { title: string, url: string }[], connection: VoiceConnection, user: User) => {
+    if (player.playingMusic) {
+        player.musicQueue.push({ title: results[0].title, url: results[0].url, requestedBy: user ?? 'unknown' });
+        player.message.channel.send(`Added \`${results[0].title}\` - To the Queue\nRequested by: ${user ?? 'unknown'}`);
+    } else {
+        const song = results[0]
+        const video = await ytdl(song.url, { filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1 << 25 });
+        player.currentSong = await ytdl.getInfo(song.url);
+        const dispatcher = connection.play(video); // first one 
+        player.playingMusic = true;
+        dispatcher?.on("finish", () => onSongFinish(player))
+            .on('debug', (debug) => console.log(debug))
+            .on('error', (error) => console.log(error))
+        player.message.channel.send(
+            `Playing \`${song.title}\` - \`${TimeFormat(parseInt(player.currentSong?.videoDetails.lengthSeconds ?? "0"))}\n\`Requested by: ${user ?? 'unknown'}`
+        )
+    }
+}
+
+
 export const onSongFinish = async (player: MusicPlayer) => {
     try {
-        console.log('onSongFinish has RAN');
         if (player.musicQueue.length > 0) {
             const next = player.musicQueue.shift();
             if (next) {
                 const connection = await player.message.member?.voice.channel?.join();
-                const video = await ytdl(next.url, { filter: 'audioonly', dlChunkSize: 0 });
+                const video = await ytdl(next.url, { filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1 << 25 });
                 player.currentSong = await ytdl.getInfo(next.url);
                 const volatileDispatcher = connection?.play(video);
-                console.log('playing next')
                 volatileDispatcher?.on('finish', () => onSongFinish(player))
-                await player.message.channel.send(
+                    .on('debug', (debug) => console.log(debug))
+                    .on('error', (error) => console.log(`error callback` + error))
+                player.message.channel.send(
                     `Playing \`${player.currentSong?.videoDetails.title}\` - \`${TimeFormat(parseInt(player.currentSong?.videoDetails.lengthSeconds ?? "0"))}\`Requested by: ${next.requestedBy ?? 'unknown'}`)
-                    // .on('start', async () => {
-                    //     await player.message.channel.send(
-                    //         `Playing \`${player.currentSong?.videoDetails.title}\` - \`${TimeFormat(parseInt(player.currentSong?.videoDetails.lengthSeconds ?? "0"))}\`Requested by: ${next.requestedBy ?? 'unknown'}`)
-                    // })
             }
         } else {
             if (player.autoplay) {
@@ -91,6 +121,8 @@ export const onSongFinish = async (player: MusicPlayer) => {
     }
 }
 
+
+
 export const TimeFormat = (seconds: number) => {
     return new Date(seconds * 1000).toISOString().substr(11, 8)
 }
@@ -98,15 +130,19 @@ export const TimeFormat = (seconds: number) => {
 export const autoplay = async (player: MusicPlayer) => {
     const related = player.currentSong?.related_videos;
     if (related) {
-        const next = "https://www.youtube.com/watch?v=" + related[0].id;
-        const connection = await player.message.member?.voice.channel?.join();
-        const video = await ytdl(next, { filter: 'audioonly', dlChunkSize: 0 });
-        player.currentSong = await ytdl.getInfo(next);
-        const volatileDispatcher = connection?.play(video);
-        volatileDispatcher?.on('finish', () => onSongFinish(player))
-            .on('start', async () => {
-                await player.message.channel.send(
-                    `Playing \`${player.currentSong?.videoDetails.title}\` - \`${TimeFormat(parseInt(player.currentSong?.videoDetails.lengthSeconds ?? "0"))}\`Requested by: ${player.message.author ?? 'unknown'}`)
-            })
+        try {
+            const next = "https://www.youtube.com/watch?v=" + related[0].id;
+            const connection = await player.message.member?.voice.channel?.join();
+            const video = await ytdl(next, { filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1 << 25 });
+            player.currentSong = await ytdl.getInfo(next);
+            const volatileDispatcher = connection?.play(video);
+            volatileDispatcher?.on('finish', () => onSongFinish(player))
+                .on('debug', (debug) => console.log(debug))
+                .on('error', (error) => console.log(`error callback` + error))
+            await player.message.channel.send(
+                `Playing \`${player.currentSong?.videoDetails.title}\` - \`${TimeFormat(parseInt(player.currentSong?.videoDetails.lengthSeconds ?? "0"))}\`Requested by: ${player.message.author ?? 'unknown'}`)
+        } catch(err) {
+            console.error(`Procedure [autoplay] error ${err}`)
+        }
     }
 }
